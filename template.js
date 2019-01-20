29bf5f96024eaf919a15495286c13b49887ba9b3
Jorge Machado <jorgepintomac@gmail.com>
2019-01-19 16:32:11 -0800

    Update template.js

--- template.js
+++ template.js
@@ -50,22 +50,17 @@ var template = '\
 <body class="tp-dync2"> \
 \
 <header class="tp-dync" align="center" style="background-color:#77ffff; font-family: serif; width:100%;"> \
-	<h1>H6nry\'s page</h1> \
-	"A different kind of magic." \
+	<h1>JorgePintoMac\'s Github page</h1> \
+	"A place to find Freedom" \
 </header> \
  \
 \
  \
 <nav class="tp-dync"><br> \
 	<a href="/index.html">Home</a><br><br> \
-	<a href="/projects.html">Projects</a><br><br> \
 	<a href="/repo/">Cydia repo</a><br><br> \
 	<a href="/tutorial-cydia-repo.html">Tutorial: How to host your own Cydia repo</a><br><br> \
-	<a href="/c-autoreleasepool.html">C-Autoreleasepool project</a><br><br> \
-	<a href="/surutcra.html">Surutcra - A Twitter client</a><br><br> \
-	<a href="/ncforward.html">NCForward - Forward your notifications</a><br><br> \
-    <a href="/common-theos-problems.html">A collection of common THEOS problems</a><br><br> \
-	<a href="http://github.com/H6nry/" target="_blank">Github</a><br><br> \
+	<a href="http://github.com/jorgepintomac/" target="_blank">Github</a><br><br> \
 </nav> \
 <aside class="tp-dync"> \
 	<h4>News</h4> \
